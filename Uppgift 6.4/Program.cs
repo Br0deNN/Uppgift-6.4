024022bcf628bf07a756249a3eac91778018278b
7R1XY <theo.broden@gmail.com>
2024-02-14 10:57:45 +0100

    Add project files.

---
+++ Uppgift 6.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv in ett tal");
+        double x = int.Parse(Console.ReadLine());
+        Console.WriteLine("Skriv in ett till tal");
+        double y = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Talet {x} upphöjt med {y} = {Potens(x, y)}");
+
+    }
+    static double Potens(double x, double y)
+    {
+        double a = 1;
+        for (int i = 0; i < y; i++)
+        {
+            a *= x;
+        }
+        return a;
+    }
+}
